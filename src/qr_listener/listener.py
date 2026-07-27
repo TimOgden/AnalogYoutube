f67ecb4b750edeaed255eaf94dd72bf546805c4d
Tim Ogden <togden1999@gmail.com>
2026-07-26 21:39:32 -0400

    Separate Dockerfiles

--- src/qr_listener/listener.py
+++ src/qr_listener/listener.py
@@ -1,64 +1,69 @@
-import httpx
-from evdev import InputDevice, categorize, ecodes
+import requests
+import serial
 from src.consts import VIDEO_ID_PATTERN
+from time import sleep
+import logging
 
-
-SCANNER_DEVICE = (
-    '/dev/input/by-id',
-    'usb-SM_SCANNER_2020-event-kbd'  # TODO: update to correct scanner name
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(message)s",
 )
+
+logger = logging.getLogger(__name__)
+
+
 PLAY_ENDPOINT = 'http://localhost:8000/api/play'
+INPUT_DEVICE = '/dev/ttyACM0'
+SLEEP_TIME = 10
 
 
-
-async def main() -> None:
-    scanner = InputDevice(SCANNER_DEVICE)
-
-    # prevent scanner keystrokes from reaching any other window
-    scanner.grab()
-
-    buffer = ''
-
-    try:
-        async for event in scanner.async_read_loop():
-            if event.type != ecodes.EV_KEY:
-                continue
-
-            key_event = categorize(event)
-            if key_event.keystate != key_event.key_down:
-                continue
-            
-            key = key_event.keycode
-            if isinstance(key, list):
-                key = key[0]
-            
-            if key == 'KEY_ENTER':
-                match = VIDEO_ID_PATTERN.fullmatch(buffer)
-                if match:
-                    video_id = match.group(1)
-
-                    async with httpx.AsyncClient() as client:
-                        await client.post(
-                            PLAY_ENDPOINT,
-                            json={'video_id': video_id},
-                            timeout=5,
-                        )
-                buffer = ''
-                continue
-            character = key_to_character(key)
-            if character is not None:
-                buffer += character
-    finally:
-        scanner.ungrab()
+def connect() -> serial.Serial:
+    while True:
+        try:
+            scanner = serial.Serial(INPUT_DEVICE, 9600)
+            logger.info("Scanner connected.")
+            return scanner
+        except serial.SerialException:
+            logger.info(
+                "Cannot find %s, retrying in %s seconds...",
+                INPUT_DEVICE,
+                SLEEP_TIME,
+            )
+            sleep(SLEEP_TIME)
 
 
-def key_to_character(key: str) -> str | None:
-    if key.startswith('KEY_'):
-        value = key.removeprefix('KEY_')
+def handle_scan(video_id: str) -> None:
+    logger.info(f'Raw scanner input: {video_id}')
+    
+    if video_id is not None and video_id != '':
+        return
+    if not VIDEO_ID_PATTERN.fullmatch(video_id):
+        logger.info('Scanner input does not match video id pattern, ignoring.')
+        return
 
-        if len(value) == 1:
-            return value.lower()
-        
-        if value == 'MINUS':
-            return '-'
-    return None
+    logger.info(f'Submitting video id `{video_id}` to FastAPI endpoint for playback.')
+    requests.post(PLAY_ENDPOINT, json={'video_id': video_id}, timeout=5)
+
+
+def main() -> None:
+    logger.info('Starting scanner service...')
+
+
+    while True:
+        scanner = connect()
+        try:
+            while True:
+                video_id = scanner.readline().decode().strip()
+                handle_scan(video_id)
+        except serial.SerialException:
+            logger.warning('Scanner disconnected.')
+
+        finally:
+            try:
+                scanner.close()
+            except Exception:
+                pass
+
+
+if __name__ == '__main__':
+    main()
