975a1d6aa8344be42d7e77448be4755f2e786079
Tim Ogden <togden1999@gmail.com>
2026-08-31 15:02:48 -0400

    Remove

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,9 +7,6 @@ cd /opt/analog-youtube
 git fetch origin
 git pull --ff-only
 
-cp "$PROJECT_DIR/deploy/analog-youtube-kiosk.desktop" \
-   "$HOME/.config/autostart/analog-youtube-kiosk.desktop"
-
 chmod +x "$PROJECT_DIR/deploy/start-kiosk.sh"
 
 docker compose build --pull
